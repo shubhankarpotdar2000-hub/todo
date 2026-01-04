02f43772e0c493e3c440bc4689fe60fb41bf128f
shubhankarpotdar2000-hub <shubhankarpotdar2000@gmail.com>
2026-01-04 21:03:41 +0530

    jjhgtg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { TodoComponent } from './shares/components/todo/todo.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
